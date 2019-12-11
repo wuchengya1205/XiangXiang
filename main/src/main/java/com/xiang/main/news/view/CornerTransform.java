42a31fbaf7218096b3b7d866a561e18c695e552f
wucy <wucy1205@yeah.net>
2019-12-11 15:25:12 +0800

    导航栏换了

--- main/src/main/java/com/xiang/main/news/view/CornerTransform.java
+++ main/src/main/java/com/xiang/main/news/view/CornerTransform.java
@@ -7,12 +7,14 @@ import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
+
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 
 import java.security.MessageDigest;
+
 
 /**
  * author : wuchengya
@@ -60,4 +62,8 @@ public class CornerTransform extends BitmapTransformation {
     }
 
 
+    @Override
+    public void updateDiskCacheKey(@NonNull MessageDigest messageDigest) {
+
+    }
 }
